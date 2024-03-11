8aeb2aa261a4f7bd0cfc656f7879bfa81f6e1e9a
Matt Watters <mwatt023@uottawa.ca>
2024-03-11 16:13:43 -0400

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Matt Watters on 2024-03-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
